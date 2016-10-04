c0300e899dc860a87da80b4f96c1cc146590116f
Ram Paul <ram@edutechsolutionsbd.com>
2016-10-04 14:33:36 +0600

    4 Oct 2016(3) / softral style sheet update

--- about.php
+++ about.php
@@ -79,42 +79,8 @@
             </section>
     </article>
 
-    <!-- Creeping text -->
-    <!--<article class="bg_red_dark">
-        <section class="wrap">
-            <div class="container-fluid">
-                <div class="col-md-12">
-                    <div class="col-md-12">
-                        <marquee>
-                            <p class="size-16 moskThin100 white no-padding no-margin"> Here Must be a Creeping Line - 1 &odot; Here Must be a Creeping Line - 2 &odot; Here Must be a Creeping Line - 3 &odot; Here Must be a Creeping Line - 4</p>
-                        </marquee>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </article>-->
-
     <!-- Bottom Boxes -->
-    <article class="bg_img">
-        <section class="bg_trans_black_30">
-            <section class="wrap">
-                <div class="container-fluid">
-                    <div class="col-md-12 bottom_box">
-                        <div class="row">
-                        <!--<div class="col-md-12 bg_trans_white_70">-->
-                            <div class="col-md-2 "><div class="box1"><img src="images/bottom_slider/ban-1.jpg" class="img-responsive"></div></div>
-                            <div class="col-md-2 "><div class="box1"><img src="images/bottom_slider/ban-2.jpg" class="img-responsive"></div></div>
-                            <div class="col-md-2 "><div class="box1"><img src="images/bottom_slider/ban-3.jpg" class="img-responsive"></div></div>
-                            <div class="col-md-2 "><div class="box1"><img src="images/bottom_slider/ban-4.jpg" class="img-responsive"></div></div>
-                            <div class="col-md-2 "><div class="box1"><img src="images/bottom_slider/ban-5.jpg" class="img-responsive"></div></div>
-                            <div class="col-md-2 "><div class="box1"><img src="images/bottom_slider/ban-6.jpg" class="img-responsive"></div></div>
-                        <!--</div>-->
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </section>
-    </article>
+    <?php include("bottom_box.php"); ?>
 
     <!-- Footer -->
     <?php include('footer.php'); ?>
--- user.php
+++ user.php
@@ -1,14 +1,6 @@
     <!-- Header-->
     <?php include('header.php'); ?>
-    <?php
-    //$arr = array();
-    //for($i=1000;$i<=1400;$i+200){
-    //for($i=10;$i<=14;$i+2){
-        //echo $i.'<br>';
-        //$arr[] = $i;
-    //}
-    //echo count($arr);
-    ?>
+    
     <!-- Body Starts From Here-->
     <article class="bg_gray_super_light">
             <!-- Content -->
@@ -251,63 +243,13 @@
                         </div>
                         <div class="h_space_20"></div><!--<div class="h_space_20"></div>-->
                     </div>
-
-                    <!-- About text Content-->
-                    <!--<div class="col-md-12">
-                        <div class="bg_gray_light padding-3-3-prcnt box-shadow-1">
-                            <h2 class="size-20 black">Software Engineers for your Business or Dream Projects</h2>
-                            <ul class="list-2 padding-3-3-prcnt">
-                                <li><i class="fa fa-check-square-o">&nbsp;</i>Softral fees are the lowest in industry.</li>
-                                <li><i class="fa fa-check-square-o">&nbsp;</i>We enroll qualified (Minimum BA or BS) Freelancer, IT professionals, with expertise from all over the world.</li>
-                                <li><i class="fa fa-check-square-o">&nbsp;</i>We invite potential employers from all industries to meet a pool of intelligent professionals</li>
-                                <li><i class="fa fa-check-square-o">&nbsp;</i>We provide and make all transactions as easy as possible</li>
-                                <li><i class="fa fa-check-square-o">&nbsp;</i>We can help Employers claim expenses by using 1099 in the USA or the equivalent in other countries</li>
-                            </ul>
-                            <h2 class="size-20 red_site">Join Softral and Grow with us. We will not let you down; that's the promise</h2>
-                        </div>
-                        <div class="h_space_20"></div><div class="h_space_20"></div>
-                    </div>-->
                 </div>
 
             </section>
     </article>
-
-    <!-- Creeping text -->
-    <!--<article class="bg_red_dark">
-        <section class="wrap">
-            <div class="container-fluid">
-                <div class="col-md-12">
-                    <div class="col-md-12">
-                        <marquee>
-                            <p class="size-16 moskThin100 white no-padding no-margin"> Here Must be a Creeping Line - 1 &odot; Here Must be a Creeping Line - 2 &odot; Here Must be a Creeping Line - 3 &odot; Here Must be a Creeping Line - 4</p>
-                        </marquee>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </article>-->
 
     <!-- Bottom Boxes -->
-    <article class="bg_img">
-        <section class="bg_trans_black_30">
-            <section class="wrap">
-                <div class="container-fluid">
-                    <div class="col-md-12 bottom_box">
-                        <div class="row">
-                        <!--<div class="col-md-12 bg_trans_white_70">-->
-                            <div class="col-md-2 "><div class="box1"><img src="images/bottom_slider/ban-1.jpg" class="img-responsive"></div></div>
-                            <div class="col-md-2 "><div class="box1"><img src="images/bottom_slider/ban-2.jpg" class="img-responsive"></div></div>
-                            <div class="col-md-2 "><div class="box1"><img src="images/bottom_slider/ban-3.jpg" class="img-responsive"></div></div>
-                            <div class="col-md-2 "><div class="box1"><img src="images/bottom_slider/ban-4.jpg" class="img-responsive"></div></div>
-                            <div class="col-md-2 "><div class="box1"><img src="images/bottom_slider/ban-5.jpg" class="img-responsive"></div></div>
-                            <div class="col-md-2 "><div class="box1"><img src="images/bottom_slider/ban-6.jpg" class="img-responsive"></div></div>
-                        <!--</div>-->
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </section>
-    </article>
+    <?php include("bottom_box.php"); ?>
 
     <!-- Footer -->
     <?php include('footer.php'); ?>
